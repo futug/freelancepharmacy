51ba7be463268dcc5fa6ead52a99fc17731c54c7
futug <102650930+futug@users.noreply.github.com>
2023-05-19 21:59:53 +0600

    optim commit

--- script.js
+++ script.js
@@ -52,47 +52,6 @@ window.addEventListener("load", () => {
   typeWriterEffect(subtitleElement, "The best pharmacy", 100);
 });
 
-const swiper = new Swiper(".swiper", {
-  slidesPerView: 4,
-
-  spaceBetween: 0,
-  centeredSlides: true,
-  // Optional parameters
-  direction: "horizontal",
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: ".next-slide",
-    prevEl: ".prev-slide",
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
-  breakpoints: {
-    1200: {
-      slidesPerView: 3,
-      spaceBetween: 0,
-    },
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 0,
-    },
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 0,
-      centeredSlides: true,
-    },
-  },
-});
-
 const slides = Array.from(document.querySelectorAll(".flowers__card"));
 
 const popup = document.getElementById("popup");
